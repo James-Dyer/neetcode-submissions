234a8f8985e74ad2aba221515f2762e83582e28f
James Dyer <68835208+James-Dyer@users.noreply.github.com>
2026-08-13 18:00:44 -0700

    Add: time-based-key-value-store - submission-7

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-7.py
@@ -0,0 +1,17 @@
+from bisect import bisect_right
+
+class TimeMap:
+
+    def __init__(self):
+        self.data = defaultdict(list) # key -> [(time, val)...]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.data[key].append((timestamp, value))
+        return
+        
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.data[key]
+        i = bisect_right(arr, timestamp, key = lambda x : x[0]) - 1
+        
+        return arr[i][1] if i != len(arr) and i >= 0 else ""
+        
